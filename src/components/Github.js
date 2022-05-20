e7dae12b6c04ff53349f3111d13a9c2f0aed402a
Serap Altunbulak <56356727+serap-altunbulak@users.noreply.github.com>
2022-05-20 06:10:46 +0300

    first commit
    repos donot show

---
+++ src/components/Github.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardMedia,
+    CardContent,
+} from '@mui/material';
+
+function Github() {
+    const [avatarURL, setAvatarURL] = useState();
+    const [githubUsername, setGitHubUsername] = useState();
+    const [repoData, setRepoData] = useState();
+
+    const repoDataURL = async () => {
+        //Get repo data about github user
+        await fetch('https://api.github.com/users/serap-altunbulak/repos')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    console.log(36, result);
+                    const list = result.map((item) => (
+                        <div className='text-center'>
+                            <a
+                                target='_blank'
+                                href={item.svn_url}
+                                rel='noreferrer'
+                            >
+                                {item.name}
+                            </a>
+                        </div>
+                    ));
+                    setRepoData(list);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    };
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/serap-altunbulak')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setAvatarURL(result.avatar_url);
+                    setGitHubUsername(result.login);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
+    return (
+        <div>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component='img'
+                        height='275'
+                        image={avatarURL}
+                        alt='avatar'
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant='h5' component='div'>
+                            {githubUsername}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size='small' color='primary' variant='outlined'>
+                        List My Repos
+                    </Button>
+                </CardActions>
+            </Card>
+            {repoData}
+        </div>
+    );
+}
+
+export default Github;
